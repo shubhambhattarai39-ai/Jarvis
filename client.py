26616e2113417e4f7a4fc1ad324e71e70c2348b9
shubhambhattarai39-ai <shubhambhattarai39@gmail.com>
2026-08-27 20:42:25 +0545

    3rd

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sent_request = False
 
 client.connect(("192.168.1.13", 5000))
+while sent_request == False:
+    
+    prompt = str(input("Enter prompt: "))
 
-prompt = str(input("Enter prompt: "))
-
-client.send(prompt.encode())
+    client.send(prompt.encode())
+    sent_request = True
 
 data = client.recv(1024).decode()
 print("Jarvis:", data)
