bee8c9dd3336e80b83cf8b3760d71a5e8edd2117
shubhambhattarai39-ai <shubhambhattarai39@gmail.com>
2026-08-27 20:46:42 +0545

    too good

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sent_request = False
 
 client.connect(("192.168.1.13", 5000))
 
-prompt = str(input("Enter prompt: "))
+while True:
+    prompt = input("You: ")
 
-client.send(prompt.encode())
-sent_request = True
+    client.send(prompt.encode())
 
-data = client.recv(1024).decode()
-print("Jarvis:", data)
+    data = client.recv(1024).decode()
+
+    print("Jarvis:", data)
+
+client.close()
