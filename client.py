3b3bd01a08edf7c21d512763a10c5f089740743a
shubhambhattarai39-ai <shubhambhattarai39@gmail.com>
2026-08-27 20:59:38 +0545

    y

--- client.py
+++ client.py
@@ -1,20 +1,41 @@
 import socket
+import speech_recognition as sr
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+recognizer = sr.Recognizer()
 
 client.connect(("192.168.1.13", 5000))
 
 while True:
-    prompt = input("You: ")
 
-    if prompt.lower() == "exit":
+    with sr.Microphone() as source:
+        print("Adjusting for background noise... Please wait.")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+    
+        print("Listening... Speak now!")
+        audio_data = recognizer.listen(source)
+        print("Processing audio...")
+
+    try:
+        prompt = recognizer.recognize_google(audio_data)
+        if prompt.lower() == "exit":
+            client.send(prompt.encode())
+            break
         client.send(prompt.encode())
-        break
+        data = client.recv(1024).decode()
+        print("Jarvis:", data)
+        
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand the audio.")
+        
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
 
-    client.send(prompt.encode())
 
-    data = client.recv(1024).decode()
 
-    print("Jarvis:", data)
+    
+   
+
+   
 
 client.close()
